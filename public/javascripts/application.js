e8113990ea402cefb8e8e16e5110efba43adcf91
Michael Moen <michael@underpantsgnome.com>
2010-12-22 19:28:53 -0600

    Added starter application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,36 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  if($('#first-focus').length > 0) {
+    $('#first-focus').focus();
+  } else {
+    $("form:not(.filter) :input:visible:enabled:first").focus();
+  }
+});
+
+var Strappy = function() {
+  return {
+    updateSortables: function(parent) {
+      var elems = $(parent + ' ul li .position'), i = 1
+      elems.each(function() {
+        this.value = i++
+      })
+    },
+
+    sizeTehToolbars: function() {
+      $('.textile-toolbar').each(function() {
+        box_id = this.id.replace('textile-toolbar-', '')
+        $(this).css('width', $('#' + box_id).css('width'))
+      })
+    },
+
+    load_gat: function(code) {
+      if (code) {
+        var host = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        jQuery.getScript(host + "google-analytics.com/ga.js", function(){
+          var tracker = _gat._getTracker(code);
+          tracker._initData();
+          tracker._trackPageview();
+        });
+      }
+    }
+  };
+}();
