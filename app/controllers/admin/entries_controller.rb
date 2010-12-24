ab3f389372084bc5569696e96cfb71071d840bf3
Michael Moen <michael@underpantsgnome.com>
2010-12-24 12:27:02 -0600

    tidy up controller

--- app/controllers/admin/entries_controller.rb
+++ app/controllers/admin/entries_controller.rb
@@ -1,48 +1,38 @@
 class Admin::EntriesController < Admin::BaseController
+  helper_method :entry
+  before_filter :set_is_draft, :only => [:create, :update]
+
   def index
     @drafts  = Entry.drafts
     @published = Entry.published
   end
 
-  def show
-    @entry = Entry.find(params[:id])
-  end
-
-  def new
-    @entry = Entry.new
-  end
-
-  def edit
-    @entry = Entry.find(params[:id])
-  end
-
   def create
-    set_is_draft
-
-    if @entry = Entry.create(params[:entry])
-      redirect_to([:admin, @entry], :notice => 'Entry was successfully created.')
+    if entry = Entry.create(params[:entry])
+      redirect_to([:admin, entry], :notice => 'Entry was successfully created.')
     else
       render :action => "new"
     end
   end
 
   def update
-    set_is_draft
-    @entry = Entry.find(params[:id])
-
-    if @entry.update_attributes(params[:entry])
-      redirect_to([:admin, @entry], :notice => 'Entry was successfully updated.')
+    if entry.update_attributes(params[:entry])
+      redirect_to([:admin, entry], :notice => 'Entry was successfully updated.')
     else
       render :action => "edit"
     end
   end
 
   def destroy
-    Entry.find(params[:id]).destroy
+    entry.destroy
     redirect_to(admin_entries_url)
   end
 
   private
+
+    def entry
+      @entry ||= params[:id].blank? ? Entry.new : Entry.find(params[:id])
+    end
 
     def set_is_draft
       params[:entry][:draft] = (params[:commit] =~ /draft/i).present?
