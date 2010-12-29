8d6c1420730f335fc45f465315a5bede56af377a
Michael Moen <michael@underpantsgnome.com>
2010-12-28 18:27:03 -0600

    add url for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101229000932) do
+ActiveRecord::Schema.define(:version => 20101229001030) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20101229000932) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   add_index "comments", ["entry_id"], :name => "index_comments_on_entry_id"
