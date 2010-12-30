2af771d090dd61f96543b3964ddd5e4be8dab310
Michael Moen <michael@underpantsgnome.com>
2010-12-30 14:40:09 -0600

    chage static asset handling

--- lib/jabe.rb
+++ lib/jabe.rb
@@ -6,8 +6,13 @@ module Jabe
   class Engine < Rails::Engine
     require File.join(JABE_DIR, 'app/helpers/jabe_helper.rb')
 
+    initializer "static assets" do |app|
+      if app.config.serve_static_assets
+        app.middleware.insert 0, ::ActionDispatch::Static, "#{root}/public"
+      end
+    end
+
     initializer "jabe setup" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
       ::ActionController::Base.send(:helper, :jabe)
     end
 
