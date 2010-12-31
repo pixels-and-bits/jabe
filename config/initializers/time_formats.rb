86db79b63fede7cc029a899bafa0ba2de2252b96
Michael Moen <michael@underpantsgnome.com>
2010-12-31 15:50:10 -0600

    fix space in time format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS[:url_part] = lambda { |time| 
   month = time.strftime('%m').gsub(/^0/, '')
-  time.strftime("%Y/#{month}/%e")
+  time.strftime("%Y/#{month}/%e").gsub(/\s+/, '')
 }
