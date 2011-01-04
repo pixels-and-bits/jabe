4b3bbd02b7ed6eeee52428f3ecb9654c2847c9c9
Michael Moen <michael@underpantsgnome.com>
2011-01-03 18:43:23 -0600

    drop grid CSS

--- lib/generators/jabe/stylesheets/stylesheets_generator.rb
+++ lib/generators/jabe/stylesheets/stylesheets_generator.rb
@@ -1,7 +1,7 @@
 module Jabe
   module Generators
     class StylesheetsGenerator < Rails::Generators::Base
-      %w( grid boilerplate handheld jabe ).each do |sheet|
+      %w( boilerplate handheld jabe ).each do |sheet|
         system("bundle exec sass #{Jabe::Engine.root}/app/stylesheets/#{sheet}.scss public/stylesheets/#{sheet}.css")
       end
     end
