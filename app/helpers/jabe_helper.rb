a96ccefade5b41a6be07b5e429e04c275cae74c4
Michael Moen <michael@underpantsgnome.com>
2011-01-05 18:58:24 -0600

    layout tweaks

--- app/helpers/jabe_helper.rb
+++ app/helpers/jabe_helper.rb
@@ -6,8 +6,12 @@ module JabeHelper
   def public_entry_url(entry)
     "#{SETTINGS.site_url}#{public_entry_path(entry)}"
   end
-  
+
   def default_gravatar_url
     "#{request.protocol}#{request.host_with_port}/images/gravatar.gif"
+  end
+
+  def page_title(title)
+    @page_title = title
   end
 end
