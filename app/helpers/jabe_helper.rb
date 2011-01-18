0c216021b042d4e190f3cab5f9feccd1572b14be
Michael Moen <michael@underpantsgnome.com>
2011-01-17 20:23:51 -0600

    content_fors

--- app/helpers/jabe_helper.rb
+++ app/helpers/jabe_helper.rb
@@ -14,4 +14,16 @@ module JabeHelper
   def page_title(title)
     @page_title = title
   end
+
+  def stylesheet(name)
+    content_for :stylesheets do
+      stylesheet_link_tag(name)
+    end
+  end
+
+  def javascript(name)
+    content_for :javascripts do
+      javascript_include_tag(name)
+    end
+  end
 end
