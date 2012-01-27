5f28db00867e1ce5e41a3568aa588cf343f76c1c
Michael Moen <michael@underpantsgnome.com>
2012-01-26 19:00:17 -0600

    pagination in feeds

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,8 +1,6 @@
 class FeedController < ApplicationController
   def index
-    @entries = Entry.published.paginate(
-      :page => (params[:page] || 1),
-      :per_page => (SETTINGS.entries_per_page || 5)
-    )
+    @entries = Entry.published.page(params[:page]).
+      per(SETTINGS.entries_per_page || 5)
   end
 end
