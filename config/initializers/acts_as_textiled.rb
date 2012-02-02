d2cab2006cd39037d57c708c000081dc7f571bf3
Michael Moen <michael@underpantsgnome.com>
2012-02-01 18:46:43 -0600

    fix jQuery/jQuery UJS issues

--- config/initializers/acts_as_textiled.rb
+++ config/initializers/acts_as_textiled.rb
@@ -4,6 +4,5 @@ rescue LoadError
   nil
 end
 
-require File.expand_path(
-  File.join(File.dirname(__FILE__), '../../lib/acts_as_textiled/base'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../../lib/acts_as_textiled/base'))
 ActiveRecord::Base.send(:include, Err::Acts::Textiled)
--- config/initializers/settings.rb
+++
@@ -1,5 +0,0 @@
-# begin
-#    Jabe::SETTINGS = Jabe::Settings.first || Jabe::Settings.create
-#    Jabe::SETTINGS.do_updates
-# rescue ActiveRecord::StatementInvalid
-# end
