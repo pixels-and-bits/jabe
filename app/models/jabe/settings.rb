866bc47d12338a2ca2aff23b5afdd338100f0a6c
Michael Moen <michael@underpantsgnome.com>
2012-10-16 23:01:01 -0500

    fix for deploy issue?

--- app/models/jabe/settings.rb
+++ app/models/jabe/settings.rb
@@ -3,8 +3,8 @@ module Jabe
     def do_updates
       Time.zone = self.time_zone
       ActionMailer::Base.default_url_options[:host] = self.host_name
-      Rakismet.key = self.akismet_key
-      Rakismet.url = self.akismet_url
+      Rakismet.key = self.akismet_key rescue nil
+      Rakismet.url = self.akismet_url rescue nil
     end
   end
 
